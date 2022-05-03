c3460763e84f70c8805ea495460598e5eea96f59
FlutLab.io <>
2022-05-03 16:09:22 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GasApp());
+} //FUNCION PRINCIPAL
+
+class GasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Gasolina',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: PaginaInicial());
+  } //widgets
+} // clase gas widget sin estado
+
+class PaginaInicial extends StatefulWidget {
+  PaginaInicial({Key? key}) : super(key: key);
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+} //widget con estado
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  List<String> images = [
+    "assets/images/compu.jpg",
+    "assets/images/compu2.jpg",
+    "assets/images/compu3.jpg",
+    "assets/images/compu4.jpg",
+    "assets/images/compu5.jpg",
+    "assets/images/compu6.jpg",
+    "assets/images/compu7.jpg",
+    "assets/images/compu.jpg",
+    "assets/images/compu7.jpg",
+    "assets/images/compu.jpg",
+    "assets/images/compu7.jpg",
+    "assets/images/compu.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Galeria de computadoras"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 10,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
